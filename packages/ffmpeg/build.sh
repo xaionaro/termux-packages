93d0ecc1a8fe33aa3c5b9fd8ccf3008f702b2eed
Dmitrii Okunev <xaionaro@dx.center>
2025-12-27 02:02:16 +0000

    ffmpeg with pulseaudio

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -3,31 +3,30 @@ TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multim
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Please align version with `ffplay` package.
-TERMUX_PKG_VERSION="8.0"
+TERMUX_PKG_VERSION="7.1.1"
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e
-TERMUX_PKG_DEPENDS="fontconfig, freetype, fribidi, game-music-emu, glslang, harfbuzz, libaom, libandroid-glob, libandroid-stub, libass, libbluray, libbz2, libdav1d, libgnutls, libiconv, liblzma, libmp3lame, libopencore-amr, libopenmpt, libopus, libplacebo, librav1e, libsoxr, libsrt, libssh, libtheora, libv4l, libvidstab, libvmaf, libvo-amrwbenc, libvorbis, libvpx, libwebp, libx264, libx265, libxml2, libzimg, libzmq, littlecms, ocl-icd, rubberband, svt-av1, vulkan-icd, xvidcore, zlib"
-TERMUX_PKG_BUILD_DEPENDS="opencl-headers, vulkan-headers"
+TERMUX_PKG_SHA256=733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1
+TERMUX_PKG_DEPENDS="fontconfig, freetype, fribidi, game-music-emu, harfbuzz, libaom, libandroid-glob, libass, libbluray, libbz2, libgnutls, libiconv, liblzma, libmp3lame, libopencore-amr, libopenmpt, libopus, libsoxr, libsrt, libssh, libv4l, libvidstab, libvmaf, libvo-amrwbenc, libvorbis, libvpx, libwebp, libx264, libx265, libxml2, libzimg, littlecms, ocl-icd, pulseaudio, svt-av1, xvidcore, zlib"
+TERMUX_PKG_BUILD_DEPENDS="opencl-headers"
 TERMUX_PKG_CONFLICTS="libav"
 TERMUX_PKG_BREAKS="ffmpeg-dev"
 TERMUX_PKG_REPLACES="ffmpeg-dev"
+TERMUX_PKG_API_LEVEL=35
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed. (These variables are also used afterwards.)
-	declare -gA _FFMPEG_SOVER=(
-		[avutil]=60
-		[avcodec]=62
-		[avformat]=62
-	)
+	_FFMPEG_SOVER_avutil=59
+	_FFMPEG_SOVER_avcodec=61
+	_FFMPEG_SOVER_avformat=61
 
-	local lib so_version
-	for lib in util codec format; do
-		so_version=$(sh ffbuild/libversion.sh av${lib} \
-				libav${lib}/version.h libav${lib}/version_major.h \
-				| sed -En 's/^libav'"${lib}"'_VERSION_MAJOR=([0-9]+)$/\1/p')
-		if [[ ! "${so_version}"  ||  "${_FFMPEG_SOVER[av${lib}]}" != "${so_version}" ]]; then
-			termux_error_exit "SOVERSION guard check failed for libav${lib}.so. expected ${so_version}"
+	local f
+	for f in util codec format; do
+		local v=$(sh ffbuild/libversion.sh av${f} \
+				libav${f}/version.h libav${f}/version_major.h \
+				| sed -En 's/^libav'"${f}"'_VERSION_MAJOR=([0-9]+)$/\1/p')
+		if [ ! "${v}" ] || [ "$(eval echo \$_FFMPEG_SOVER_av${f})" != "${v}" ]; then
+			termux_error_exit "SOVERSION guard check failed for libav${f}.so. expected ${v}"
 		fi
 	done
 }
@@ -36,34 +35,24 @@ termux_step_configure() {
 	cd $TERMUX_PKG_BUILDDIR
 
 	local _EXTRA_CONFIGURE_FLAGS=""
-	case "$TERMUX_ARCH" in
-		"aarch64")
-			_ARCH="$TERMUX_ARCH"
-			_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-		;;
-		"arm")
-			_ARCH="armeabi-v7a"
-			_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-			# use '-Wno-error=incompatible-pointer-types' with 32-bit ARM target to work around
-			# error: incompatible function pointer types initializing
-			# 'PFN_vkDebugUtilsMessengerCallbackEXT'... with an expression of type 'VkBool32`...
-			# following the example of the Arch Linux AUR lib32-ffmpeg package:
-			# https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=lib32-ffmpeg&id=41476d610980376bcbe054ee183f46705be27747#n171
-			CFLAGS+=" -Wno-error=incompatible-pointer-types"
-		;;
-		"i686")
-			_ARCH="x86"
-			# Specify --disable-asm to prevent text relocations on i686,
-			# see https://trac.ffmpeg.org/ticket/4928
-			_EXTRA_CONFIGURE_FLAGS="--disable-asm"
-		;;
-		"x86_64")
-			_ARCH="x86_64"
-		;;
-		*) termux_error_exit "Unsupported arch: $TERMUX_ARCH";;
-	esac
+	if [ $TERMUX_ARCH = "arm" ]; then
+		_ARCH="armeabi-v7a"
+		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
+	elif [ $TERMUX_ARCH = "i686" ]; then
+		_ARCH="x86"
+		# Specify --disable-asm to prevent text relocations on i686,
+		# see https://trac.ffmpeg.org/ticket/4928
+		_EXTRA_CONFIGURE_FLAGS="--disable-asm"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+		_ARCH="x86_64"
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+		_ARCH=$TERMUX_ARCH
+		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
+	else
+		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
+	fi
 
-	$TERMUX_PKG_SRCDIR/configure \
+	CFLAGS="$CFLAGS -Wno-incompatible-function-pointer-types" $TERMUX_PKG_SRCDIR/configure \
 		--arch="${_ARCH}" \
 		--as="$AS" \
 		--cc="$CC" \
@@ -76,74 +65,80 @@ termux_step_configure() {
 		--cross-prefix="${TERMUX_HOST_PLATFORM}-" \
 		--disable-indevs \
 		--disable-outdevs \
+		--enable-indev=android_camera \
 		--enable-indev=lavfi \
-		--disable-static \
+		--enable-indev=v4l2 \
+		--enable-indev=pulse \
+		--enable-indev=alsa \
+		--enable-outdev=pulse \
+		--enable-outdev=v4l2 \
+		--enable-outdev=alsa \
+		--enable-static \
 		--disable-symver \
 		--enable-cross-compile \
-		--enable-gnutls \
+		--disable-gnutls \
 		--enable-gpl \
 		--enable-version3 \
 		--enable-jni \
-		--enable-lcms2 \
-		--enable-libaom \
-		--enable-libass \
-		--enable-libbluray \
-		--enable-libdav1d \
-		--enable-libfontconfig \
-		--enable-libfreetype \
-		--enable-libfribidi \
-		--enable-libglslang \
-		--enable-libgme \
-		--enable-libharfbuzz \
-		--enable-libmp3lame \
-		--enable-libopencore-amrnb \
-		--enable-libopencore-amrwb \
-		--enable-libopenmpt \
+		--disable-lcms2 \
+		--disable-libaom \
+		--disable-libass \
+		--disable-libbluray \
+		--disable-libdav1d \
+		--disable-libfontconfig \
+		--disable-libfreetype \
+		--disable-libfribidi \
+		--disable-libgme \
+		--disable-libharfbuzz \
+		--disable-libmp3lame \
+		--disable-libopencore-amrnb \
+		--disable-libopencore-amrwb \
+		--disable-libopenmpt \
+		--enable-libpulse \
 		--enable-libopus \
-		--enable-libplacebo \
-		--enable-librav1e \
-		--enable-librubberband \
-		--enable-libsoxr \
+		--disable-librav1e \
+		--disable-librubberband \
+		--disable-libsoxr \
 		--enable-libsrt \
-		--enable-libssh \
-		--enable-libsvtav1 \
-		--enable-libtheora \
+		--disable-libssh \
+		--disable-libsvtav1 \
+		--disable-libtheora \
 		--enable-libv4l2 \
-		--enable-libvidstab \
-		--enable-libvmaf \
-		--enable-libvo-amrwbenc \
-		--enable-libvorbis \
-		--enable-libvpx \
-		--enable-libwebp \
+		--disable-libvidstab \
+		--disable-libvmaf \
+		--disable-libvo-amrwbenc \
+		--disable-libvorbis \
+		--disable-libvpx \
+		--disable-libwebp \
 		--enable-libx264 \
 		--enable-libx265 \
-		--enable-libxml2 \
-		--enable-libxvid \
-		--enable-libzimg \
-		--enable-libzmq \
+		--disable-libxml2 \
+		--disable-libxvid \
+		--disable-libzimg \
+		--disable-bzlib \
+		--disable-xlib \
 		--enable-mediacodec \
-		--enable-opencl \
+		--disable-opencl \
 		--enable-shared \
 		--prefix="$TERMUX_PREFIX" \
 		--target-os=android \
-		--extra-libs="-landroid-glob" \
-		--enable-vulkan \
+		--disable-vulkan \
 		$_EXTRA_CONFIGURE_FLAGS \
 		--disable-libfdk-aac
 	# GPLed FFmpeg binaries linked against fdk-aac are not redistributable.
 }
 
 termux_step_post_massage() {
-	cd "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib" || termux_error_exit "couldn't symlink shared libraries."
-	local lib so_version
-	for lib in util codec format; do
-		so_version="${_FFMPEG_SOVER[av${lib}]}"
-		if [[ ! "${so_version}" ]]; then
-			termux_error_exit "Empty SOVERSION for libav${lib}."
+	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
+	local f
+	for f in util codec format; do
+		local s=$(eval echo \$_FFMPEG_SOVER_av${f})
+		if [ ! "${s}" ]; then
+			termux_error_exit "Empty SOVERSION for libav${f}."
 		fi
 		# SOVERSION suffix is expected by some programs, e.g. Firefox.
-		if [[ ! -e "./libav${lib}.so.${so_version}" ]]; then
-			ln -sf "libav${lib}.so" "libav${lib}.so.${so_version}"
+		if [ ! -e "./libav${f}.so.${s}" ]; then
+			ln -sf libav${f}.so libav${f}.so.${s}
 		fi
 	done
 }
